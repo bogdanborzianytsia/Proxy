cdd623f519e01c16c650eea67db6946547485ee5
bogdanborzianytsia <128235605+bogdanborzianytsia@users.noreply.github.com>
2023-03-23 17:44:31 +0200

    Add files via upload

---
+++ Prroxy/Prroxy/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prroxy
+{
+    public interface IDatabase
+    {
+        void Query(string command);
+    }
+    public class Database : IDatabase
+    {
+        public void Query(string command)
+        {
+            MessageBox.Show($"Executing command: {command}");
+        }
+    }
+
+    public class DatabaseProxy : IDatabase
+    {
+        private Database _realDatabase;
+        private bool _isAdmin;
+
+        public DatabaseProxy(bool isAdmin)
+        {
+            _isAdmin = isAdmin;
+        }
+
+        public void Query(string command)
+        {
+            if (_realDatabase == null)
+            {
+                _realDatabase = new Database();
+            }
+
+            if (_isAdmin)
+            {
+                _realDatabase.Query(command);
+            }
+            else
+            {
+                if (command.StartsWith("SELECT"))
+                {
+                    _realDatabase.Query(command);
+                }
+                else
+                {
+                    MessageBox.Show("Access denied. Only SELECT commands are allowed for non-admin users.");
+                }
+            }
+        }
+    }
+}
---
+++ Prroxy/Prroxy/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prroxy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IDatabase database = new DatabaseProxy(false);
+
+            database.Query("SELECT * FROM users"); // 
+            database.Query("INSERT INTO users (name, email) VALUES ('example', 'example@example.com')"); 
+
+            IDatabase databaseAdmin = new DatabaseProxy(true);
+
+            databaseAdmin.Query("SELECT * FROM users"); 
+            databaseAdmin.Query("INSERT INTO users (name, email) VALUES ('example', 'example@example.com')"); 
+        }
+    }
+}
